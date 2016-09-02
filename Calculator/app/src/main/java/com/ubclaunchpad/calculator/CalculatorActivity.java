30a9cde6c1a3a9aee22a794044d7a25eca529cfc
Kelvin Chan <chan.kelv@gmail.com>
2016-09-02 14:59:29 -0700

    Simple Calculator skeleton

--- Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
+++ Calculator/app/src/main/java/com/ubclaunchpad/calculator/CalculatorActivity.java
@@ -2,12 +2,56 @@ package com.ubclaunchpad.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
+    private final static String TAG = CalculatorActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    /**
+     * This implementation of the click listener is for the view found in res/layout/activity_calculator
+     * The functions in general should grab the appropriate inputs, and if they are valid, computes the answer.
+     * The answer should be displayed in a second activity labelled "AnswerActivity"
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.operation_add:
+            {
+                //TODO add function
+                break;
+            }
+            case R.id.operation_sub:
+            {
+                //TODO subtract function
+                break;
+            }
+            case R.id.operation_multi:
+            {
+                //TODO multiply function
+                break;
+            }
+            case R.id.operation_div:
+            {
+                //TODO divide function
+                break;
+            }
+            //TODO any extra implmentations (optional)
+            default:
+            {
+                Toast.makeText(this, "Click not implmented yet", Toast.LENGTH_LONG).show();
+                Log.e(TAG, "View id: " + v.getId() + " click not implemented yet");
+                break;
+            }
+        }
     }
 }
